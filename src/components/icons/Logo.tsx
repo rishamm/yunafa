4883ab7d967d0b0ac0713cbbdb3bf2b6e3236577
MUHAMMED RISHAM <risham45334@gmail.com>
2025-06-09 07:02:30 +0000

    change the font

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -15,7 +15,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
         y="50%"
         dominantBaseline="middle"
         textAnchor="middle"
-        fontFamily="Belleza, sans-serif"
+        fontFamily="Playfair Display, serif"
         fontSize="24"
         fill="currentColor"
       >
