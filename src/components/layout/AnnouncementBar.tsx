ea801cd743635f29c4173cc78da70a72d1b85add
MUHAMMED RISHAM <risham45334@gmail.com>
2025-06-17 14:31:52 +0000

    remove the bg of the  header

--- src/components/layout/AnnouncementBar.tsx
+++ src/components/layout/AnnouncementBar.tsx
@@ -5,25 +5,25 @@ export function AnnouncementBar() {
   return (
     <div className="bg-card py-2 px-4 text-center text-sm font-medium overflow-hidden">
       <div className="inline-flex whitespace-nowrap animate-marquee-left">
-        <p className="mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
+        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
           Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
           <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
             Explore Now
           </Link>
         </p>
-        <p className="mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
+        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
           Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
           <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
             Explore Now
           </Link>
         </p>
-        <p className="mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
+        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
           Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
           <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
             Explore Now
           </Link>
         </p>
-        <p className="mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
+        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
           Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
           <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
             Explore Now
