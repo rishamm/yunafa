2ba986bacc2344717ff3e5984e93b8e78768ddc9
MUHAMMED RISHAM <risham45334@gmail.com>
2025-06-21 10:47:43 +0000

    increase the height of the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,16 @@
 
 export function Footer() {
   return (
-    <footer className="bg-muted/50 border-t mt-auto overflow-hidden">
-      <div className="container mx-auto px-4 pt-8 text-sm text-muted-foreground">
+    <footer className="bg-muted/50  border-t mt-auto overflow-hidden">
+      <div className="container mx-auto h-full px-4 flex flex-col gap-2 justify-center py-8 text-sm text-muted-foreground">
         {/* This div will align its content (the p tag) to the left */}
         <div className="text-left"> 
           <p className="mb-2">&copy; {new Date().getFullYear()} Yunafa. All rights reserved.</p>
         </div>
-        <div className="text-center"> {/* This div will center the large YUNAFA text */}
-          <span className="text-7xl md:text-[15.5rem] font-bold font-headline text-white leading-[0]">YUNAFA</span>
+        <div className="text-center h-full"> {/* This div will center the large YUNAFA text */}
+          <span className="text-7xl md:text-[15.5rem] font-bold text-white leading-[0]">YUNAFA</span>
         </div>
       </div>
     </footer>
   );
 }
-
