3900bc8eaeb0b97a7ba896fdc6d581c95cefa578
MUHAMMED RISHAM <risham45334@gmail.com>
2025-06-23 13:40:53 +0000

    .magicpattern {   width: 100%;   height: 100%;   background-size: cover

--- src/components/layout/AnnouncementBar.tsx
+++ src/components/layout/AnnouncementBar.tsx
@@ -1,36 +1,6 @@
 
-import Link from 'next/link';
-
 export function AnnouncementBar() {
   return (
-    <div className="bg-card py-2 px-4 text-center text-sm font-medium overflow-hidden">
-      <div className="inline-flex whitespace-nowrap animate-marquee-left">
-        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
-          Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
-          <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
-            Explore Now
-          </Link>
-        </p>
-        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
-          Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
-          <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
-            Explore Now
-          </Link>
-        </p>
-        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
-          Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
-          <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
-            Explore Now
-          </Link>
-        </p>
-        <p className="font-semibold mx-10 md:mx-20 text-md flex-shrink-0 text-announcement">
-          Special Announcement: Yunafa&apos;s New Collection Just Dropped!{" "}
-          <Link href="/#home-carousel" className="font-semibold underline text-announcement hover:opacity-80 transition-opacity">
-            Explore Now
-          </Link>
-        </p>
-      </div>
-    </div>
+    <div className="magicpattern h-12 w-full" />
   );
 }
-
