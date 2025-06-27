b4366bcf51bd9304ce8298310e95b2437c3fbfd8
MUHAMMED RISHAM <risham45334@gmail.com>
2025-06-27 14:43:07 +0000

    make it move fast

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         marquee: 'marquee 25s linear infinite',
-        'wave-scroll': 'wave-scroll 40s linear infinite',
+        'wave-scroll': 'wave-scroll 20s linear infinite',
       },
     },
   },
